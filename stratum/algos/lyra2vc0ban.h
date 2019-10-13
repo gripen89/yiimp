749312cda5fc51258e4fa26904b8e2fa5a2c4fff
gripen89 <47283478+gripen89@users.noreply.github.com>
2019-10-13 04:21:25 +0300

    Create lyra2vc0ban.h

---
+++ stratum/algos/lyra2vc0ban.h
@@ -0,0 +1,14 @@
+#ifndef LYRA2VC0BAN_H
+#define LYRA2VC0BAN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void lyra2rec0ban_hash(const char* input, char* output, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
